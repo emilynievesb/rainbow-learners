caff3a1a53a24f6064a9df4becaf78d35d3924f4
Emily Nieves <nemily1710@gmail.com>
2025-04-15 21:29:31 -0500

    refactor: :recycle: refactor colors component

--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { ColorService, Color } from '../services/color.service';
 
 @Component({
   selector: 'app-colors',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatCardModule, MatButtonModule],
   templateUrl: './colors.component.html',
   styleUrls: ['./colors.component.scss'],
 })
