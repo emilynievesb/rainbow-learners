8e53b6480a09cbc24c2ba24f3a866533d1acb70f
Emily Nieves <nemily1710@gmail.com>
2025-04-15 20:32:54 -0500

    refactor: :recycle: refactor home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
