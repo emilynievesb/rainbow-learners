301eeac40f7c7eedd267eb5407fb84620043f7bf
Emily Nieves <nemily1710@gmail.com>
2025-04-15 20:28:59 -0500

    feat: :sparkles: add routes
    add the routes to display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ColorsComponent } from './colors/colors.component';
+import { ColorDetailComponent } from './color-detail/color-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'colors', component: ColorsComponent },
+  { path: 'colors/:id', component: ColorDetailComponent },
+  // Ruta comodín para redirigir a la página de inicio si la ruta no existe
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
