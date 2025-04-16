f845b67918a6f83d7a81d595a775ef3cc2b57014
Emily Nieves <nemily1710@gmail.com>
2025-04-15 21:37:16 -0500

    refactor: :recycle: refacto color detail component

--- src/app/color-detail/color-detail.component.ts
+++ src/app/color-detail/color-detail.component.ts
@@ -2,29 +2,29 @@ import { Component, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ColorService, Color } from '../services/color.service';
+import { MatCard, MatCardActions } from '@angular/material/card';
 
 @Component({
   selector: 'app-color-detail',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatCardActions, MatCard],
   templateUrl: './color-detail.component.html',
   styleUrls: ['./color-detail.component.scss'],
 })
 export class ColorDetailComponent implements OnInit {
-  // Creamos una signal para almacenar el color seleccionado
+  // Signal para almacenar el color seleccionado
   color = signal<Color | undefined>(undefined);
 
   constructor(
     private route: ActivatedRoute,
-    private colorService: ColorService // Inyectamos el servicio
+    private colorService: ColorService
   ) {}
 
   ngOnInit() {
-    // Obtenemos el parámetro 'id' de la URL
+    // Recuperamos el parámetro "id" de la ruta y buscamos el color en el servicio
     const idParam = this.route.snapshot.paramMap.get('id');
     const id = idParam ? parseInt(idParam, 10) : null;
     if (id !== null) {
-      // Utilizamos el método del servicio para buscar el color
       const foundColor = this.colorService.getColorById(id);
       if (foundColor) {
         this.color.set(foundColor);
