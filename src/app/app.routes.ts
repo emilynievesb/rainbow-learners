395630287a2032559eb9f3581602e35cc3837a99
Emily Nieves <nemily1710@gmail.com>
2025-04-15 21:43:09 -0500

    feat: :sparkles: integration with the game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ColorsComponent } from './colors/colors.component';
 import { ColorDetailComponent } from './color-detail/color-detail.component';
+import { GameComponent } from './game/game.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'colors', component: ColorsComponent },
   { path: 'colors/:id', component: ColorDetailComponent },
+  { path: 'game', component: GameComponent },
   // Ruta comodín para redirigir a la página de inicio si la ruta no existe
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
